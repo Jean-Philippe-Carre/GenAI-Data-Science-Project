18dc918f3ee1cc6806061e9047c3d873510f970b
Jean-Philippe-Carre <jp.carre@hotmail.fr>
2024-11-19 15:30:51 +0400

    flask static templates folder 1

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'Images/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
